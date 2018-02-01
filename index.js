70aa638bb78249090cfedff859c96e95cf3ee31d
Carla Walter <walterc@gmx.com>
2018-02-01 06:56:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function happyHolidays() {return ('Happy holidays!')}
 function happyHolidaysTo(name) {return `Happy holidays, ${name}!`}
 function happyHolidayTo(holiday,name) {return `Happy ${holiday}, ${name}!`}
-function holidayCountdown(days,holiday) {return `It's ${days} until ${holiday}!`}
+function holidayCountdown(holiday,days) {return `It's ${days} until ${holiday}!`}
